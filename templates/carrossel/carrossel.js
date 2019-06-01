ba7e1242797024576b9376eb0b424ba05050237f
johnatasf <johnatas.sf@gmail.com>
2019-06-01 13:26:20 -0300

    Adicionado exemplo para o carrossel

--- templates/carrossel/carrossel.js
+++ templates/carrossel/carrossel.js
@@ -5,31 +5,21 @@
 var esquerda = document.querySelector(".esquerda");
 var direita = document.querySelector(".direita");
 var nav = document.querySelector(".nav");
-var tamanhoMaximo =  Math.round(document.querySelectorAll(".foto").length/3)
-var contador = 1;
-var tamanhoAtual = contador * 300;
+var contador = 0;
 
 
 function transactionDireita() {
-    if (tamanhoAtual > tamanhoMaximo) {
-        nav.style.left = "-"+ 300 * contador + "px"
-        contador++;
-        tamanhoAtual +=300;
-    } else {
-        contador = 0;
-        nav.style.left = "0px"
+    contador++;
+    if (contador >= 1 && contador != 2) {
+        nav.style.left = "-"+ 50 * contador + "%"
     }
 
 }
 function transactionEsquerda() {
-    if (contador == 1) {
-        nav.style.left = '-'+300 * tamanhoMaximo + "px"
-        contador = tamanhoMaximo;
-    } else {
-        contador--;
-        nav.style.left = 300 * contador + "px"
-        contador++;
-        tamanhoAtual= 300 * contador;
+    contador--;
+    if (contador != 1 && contador <= 2) {
+        nav.style.left = 50 * contador + "%"
+       
     }
 }
 
@@ -39,9 +29,3 @@ esquerda.addEventListener('click', function (ev) {
 direita.addEventListener('click', function (ev) {
     transactionDireita()
 });
-// });
-// navegar(0);
-// .onclick = function (e) {
-//     e.preventDefault();
-//     menuLateral.style.marginLeft = "0";
-// };
